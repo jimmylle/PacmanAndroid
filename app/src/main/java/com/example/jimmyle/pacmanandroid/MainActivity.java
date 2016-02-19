56b4ccc75b8d09cb5e87a275de21114ee6be6d15
jimmy le <lejimmyl@yahoo.com>
2016-02-18 22:51:35 -0800

    Merge branch 'master' of /Users/Jimmyle/AndroidStudioProjects/PacmanAndroid with conflicts.

--- app/src/main/java/com/example/jimmyle/pacmanandroid/MainActivity.java
+++ app/src/main/java/com/example/jimmyle/pacmanandroid/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.jimmyle.pacmanandroid;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    MediaPlayer player;
 
     // Method to start activity for Help button
     public void showHelpScreen(View view) {
@@ -19,6 +19,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //startService(new Intent(this, BackgroundMusicService.class));
+        player = MediaPlayer.create(this,R.raw.pacman_song);
+        player.setLooping(true);
+        player.setVolume(100,100);
+        player.start();
     }
 
 //    @Override
@@ -42,4 +47,16 @@ public class MainActivity extends AppCompatActivity {
 //
 //        return super.onOptionsItemSelected(item);
 //    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        stopService(new Intent(this,BackgroundMusicService.class));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        startService(new Intent(this, BackgroundMusicService.class));
+    }
 }
