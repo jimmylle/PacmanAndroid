649452516ee09d6d0000af5e41b4e4976d127761
Kevin <nivek325@gmail.com>
2016-02-19 13:25:52 -0800

    Add Play Activity for the play button

---
+++ app/src/main/java/com/example/jimmyle/pacmanandroid/PlayActivity.java
@@ -0,0 +1,14 @@
+package com.example.jimmyle.pacmanandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class PlayActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View view = new MyView(this);
+        setContentView(view);
+    }
+}
