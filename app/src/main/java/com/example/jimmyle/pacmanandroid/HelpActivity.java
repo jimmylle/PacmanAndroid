f3b47a65b52fe0575219243bb4a44fbb13ed417b
Kevin <nivek325@gmail.com>
2016-02-18 22:50:50 -0800

    Remove music

--- app/src/main/java/com/example/jimmyle/pacmanandroid/HelpActivity.java
+++ app/src/main/java/com/example/jimmyle/pacmanandroid/HelpActivity.java
@@ -10,18 +10,5 @@ public class HelpActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.help_layout);
-        startService(new Intent(this, BackgroundMusicService.class));
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        stopService(new Intent(this, BackgroundMusicService.class));
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        startService(new Intent(this, BackgroundMusicService.class));
     }
 }
--- app/src/main/java/com/example/jimmyle/pacmanandroid/MainActivity.java
+++ app/src/main/java/com/example/jimmyle/pacmanandroid/MainActivity.java
@@ -19,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startService(new Intent(this, BackgroundMusicService.class));
     }
 
 //    @Override
@@ -43,16 +42,4 @@ public class MainActivity extends AppCompatActivity {
 //
 //        return super.onOptionsItemSelected(item);
 //    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        stopService(new Intent(this,BackgroundMusicService.class));
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        startService(new Intent(this, BackgroundMusicService.class));
-    }
 }
