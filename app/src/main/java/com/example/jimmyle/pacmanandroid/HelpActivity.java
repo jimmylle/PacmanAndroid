e65e9be8d7e84944505448bf8ebb1dede85b9d3a
Kevin <nivek325@gmail.com>
2016-02-19 20:54:02 -0800

    Change colors in help_layout

--- app/src/main/java/com/example/jimmyle/pacmanandroid/HelpActivity.java
+++ app/src/main/java/com/example/jimmyle/pacmanandroid/HelpActivity.java
@@ -1,11 +1,9 @@
 package com.example.jimmyle.pacmanandroid;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 
 public class HelpActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
