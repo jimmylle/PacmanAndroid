cf40757cbb43f62c51a1e2dfedf91b64496a6296
jimmy le <lejimmyl@yahoo.com>
2016-02-20 20:16:19 -0800

    Add Framelayout to play activity so that there are overlapping layers

--- app/src/main/java/com/example/jimmyle/pacmanandroid/PlayActivity.java
+++ app/src/main/java/com/example/jimmyle/pacmanandroid/PlayActivity.java
@@ -3,12 +3,18 @@ package com.example.jimmyle.pacmanandroid;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.FrameLayout;
 
 public class PlayActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        View view = new InteractiveView(this);
-        setContentView(view);
+        FrameLayout frameLayout = new FrameLayout(this);
+        View mapView = new MapView(this);
+        View interactiveView = new InteractiveView(this);
+        frameLayout.addView(mapView);
+        frameLayout.addView(interactiveView);
+        setContentView(frameLayout);
     }
 }
