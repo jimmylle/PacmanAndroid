7aad54c644592a6f274357f3d28182b03b1da9a3
jimmy le <lejimmyl@yahoo.com>
2016-02-26 22:28:46 -0800

    Fix high score bug
    There was a bug where if you killed/destroyed the app (swiped it away in the recent apps list) the high score would not be saved the next time you started the app. This fixes that bug so that it keeps the same high score as long as you don't uninstall the app.

--- app/src/main/java/com/example/jimmyle/pacmanandroid/PlayActivity.java
+++ app/src/main/java/com/example/jimmyle/pacmanandroid/PlayActivity.java
@@ -1,12 +1,15 @@
 package com.example.jimmyle.pacmanandroid;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 
 public class PlayActivity extends Activity {
     static PlayActivity activity;
+    private SharedPreferences sharedPreferences;
     private DrawingView drawingView;
+    private Globals globals;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,14 +17,20 @@ public class PlayActivity extends Activity {
         drawingView = new DrawingView(this);
         setContentView(drawingView);
         activity = this;
+        globals = Globals.getInstance();
+        sharedPreferences = getSharedPreferences("info", MODE_PRIVATE);
+        int temp = sharedPreferences.getInt("high_score",0);
+        globals.setHighScore(temp);
     }
-
 
     @Override
     protected void onPause() {
         Log.i("info", "onPause");
         super.onPause();
         drawingView.pause();
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt("high_score", globals.getHighScore());
+        editor.apply();
     }
 
     @Override
@@ -34,4 +43,5 @@ public class PlayActivity extends Activity {
     public static PlayActivity getInstance() {
         return activity;
     }
+
 }
